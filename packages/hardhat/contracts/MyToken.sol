b4e65c84273197531d51a45f465b5cca3951bcfd
cameronmcewan <cm17521@bristol.ac.uk>
2023-11-20 13:16:13 +0000

    MyToken.sol deployment success

--- packages/hardhat/contracts/MyToken.sol
+++ packages/hardhat/contracts/MyToken.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
+contract MyToken is ERC20, AccessControl, ERC20Votes {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
